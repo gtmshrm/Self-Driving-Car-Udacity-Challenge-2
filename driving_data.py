a695da3497465cafde1fc01c844e2af145978865
Gautam Sharma <gautamsharma@Gautams-MacBook-Air.local>
2016-11-29 22:19:24 +0530

    update directory structure

--- driving_data.py
+++ driving_data.py
@@ -1,8 +1,11 @@
+from __future__ import with_statement
 import scipy.misc
 import random
-
-# augmentation of image
-augmentation = "center"
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import cv2
 
 xs = []
 ys = []
@@ -10,15 +13,11 @@ ys = []
 # points to the end of the last batch
 train_batch_pointer = 0
 val_batch_pointer = 0
-
-# read data.txt
-with open("/Volumes/Untitled/h5imgs/"+augmentation+"/data.txt") as f:
-    for line in f:
-        xs.append(line.split()[0])
-        # the paper by Nvidia uses the inverse of the turning radius,
-        # but steering wheel angle is proportional to the inverse of turning radius
-        # so the steering wheel angle in radians is used as the output
-        ys.append(float(line.split()[1]))
+FDIR='/media/gautam/147DA6412B0B84FB/Udacity datasets/challenge 2 training/Ch2_002/images & interpolated data/'
+df = pd.read_csv('/media/gautam/147DA6412B0B84FB/Udacity datasets/challenge 2 training/Ch2_002/images & interpolated data/interpolated.csv')
+df=df[df.frame_id=='center_camera']
+xs = [FDIR + s for s in df.filename]
+ys = df.angle
 
 # get number of images
 num_images = len(xs)
@@ -28,31 +27,34 @@ c = list(zip(xs, ys))
 random.shuffle(c)
 xs, ys = zip(*c)
 
-train_xs = xs[:int(len(xs) * 0.8)]
-train_ys = ys[:int(len(xs) * 0.8)]
 
-val_xs = xs[-int(len(xs) * 0.2):]
-val_ys = ys[-int(len(xs) * 0.2):]
+train_xs = xs[:int(len(xs) * 0.7)]
+train_ys = ys[:int(len(xs) * 0.7)]
+
+val_xs = xs[-int(len(xs) * 0.3):]
+val_ys = ys[-int(len(xs) * 0.3):]
 
 num_train_images = len(train_xs)
 num_val_images = len(val_xs)
+
 
 def LoadTrainBatch(batch_size):
     global train_batch_pointer
     x_out = []
     y_out = []
     for i in range(0, batch_size):
-        x_out.append(scipy.misc.imresize(scipy.misc.imread(train_xs[(train_batch_pointer + i) % num_train_images])[-150:], [66, 200]) / 255.0)
+        x_out.append(scipy.misc.imresize (  (scipy.misc.imread(train_xs[(train_batch_pointer + i) % num_train_images])[240:,:]) , [120, 160] )/ 255.0)
         y_out.append([train_ys[(train_batch_pointer + i) % num_train_images]])
     train_batch_pointer += batch_size
     return x_out, y_out
+
 
 def LoadValBatch(batch_size):
     global val_batch_pointer
     x_out = []
     y_out = []
     for i in range(0, batch_size):
-        x_out.append(scipy.misc.imresize(scipy.misc.imread(val_xs[(val_batch_pointer + i) % num_val_images])[-150:], [66, 200]) / 255.0)
+        x_out.append(scipy.misc.imresize ( (scipy.misc.imread(val_xs[(val_batch_pointer + i) % num_val_images])[240:,:]), [120, 160] )/ 255.0)
         y_out.append([val_ys[(val_batch_pointer + i) % num_val_images]])
     val_batch_pointer += batch_size
     return x_out, y_out
---
+++ train.py
@@ -0,0 +1,58 @@
+'''
+To view tensorboard train and validation loss
+In the 1st terminal, run the command line:
+	--> tensorboard --logdir=./train_logs
+	Then open http://0.0.0.0:6006/ into your web browser
+In the 2nd terminal, run the command line:
+	--> tensorboard --logdir=./val_logs --port=8008"
+	Then open http://0.0.0.0:8008/ into your web browser
+'''
+import os
+import os.path
+import tensorflow as tf
+import driving_data
+import model
+
+if 
+	LOGDIR = './save'
+	CKPT_FILE = './save/model.ckpt'
+	TRAIN_TENSORBOARD_LOG = './train_logs'
+	VAL_TENSORBOARD_LOG = './val_logs'
+
+	sess = tf.InteractiveSession()
+
+	loss = tf.reduce_mean(tf.square(tf.sub(model.y_, model.y)))
+	train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+
+	saver = tf.train.Saver()
+
+	# create a summary to monitor cost tensor
+	train_summary = tf.scalar_summary("train_loss", loss)
+	val_summary = tf.scalar_summary("val_loss", loss)
+
+	if not os.path.exists(LOGDIR):
+		os.makedirs(LOGDIR)
+
+	if os.path.isfile(CKPT_FILE):
+		saver.restore(sess, CKPT_FILE)
+	else:
+		sess.run(tf.initialize_all_variables())
+
+	if not os.path.exists(TRAIN_TENSORBOARD_LOG):
+		os.makedirs(TRAIN_TENSORBOARD_LOG)
+	if not os.path.exists(VAL_TENSORBOARD_LOG):
+		os.makedirs(VAL_TENSORBOARD_LOG)
+
+	# op to write logs to Tensorboard
+	train_summary_writer = tf.train.SummaryWriter(TRAIN_TENSORBOARD_LOG, graph=tf.get_default_graph())
+	val_summary_writer = tf.train.SummaryWriter(VAL_TENSORBOARD_LOG, graph=tf.get_default_graph())
+
+	for i in range(int(driving_data.num_images * 3)):
+		xs, ys = driving_data.LoadTrainBatch(100)
+		train_step.run(feed_dict={model.x: xs, model.y_: ys, model.keep_prob: 0.8})
+		if i % 10 == 0:
+			xs, ys = driving_data.LoadValBatch(100)
+			print("step %d, val loss %g"%(i, loss.eval(feed_dict={model.x:xs, model.y_: ys, model.keep_prob: 1.0})))
+		if i % 100 == 0:
+			filename = saver.save(sess, CKPT_FILE)
+			print("Model saved in file: %s" % filename)
